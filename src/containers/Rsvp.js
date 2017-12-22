ba9a283cbaf44d460c6d6bf257d3fa1c103342f5
Patrick Kosnik <patrickkosnik@patricks-mbp.attlocal.net>
2017-12-22 14:45:01 -0500

    testing out some stuff for rsvp

--- src/containers/Rsvp.js
+++ src/containers/Rsvp.js
@@ -8,6 +8,19 @@ const Rsvp = () => {
         <div className={styles.container}>
             <Header light />
             <div className={styles.banner}>Coming Soon</div>
+            <div>{guests.guests.map(guest => 
+            <div>
+                First Name: {guest.firstName} <br />
+                Last Name: {guest.lastName} <br />
+                { guest.partnerFirst ? <div>First Name: {guest.partnerFirst}<br /></div> : ''}
+                { guest.partnerFirst ? <div>Last Name: {guest.partnerLast}<br /></div> : ''}
+                Plus Ones: {guest.plusOnes.map(
+                    plusOne => <span>{plusOne}, </span>
+                )
+                }
+                <br /><br />
+            </div>)
+        }</div>
         </div>
     )
 }
